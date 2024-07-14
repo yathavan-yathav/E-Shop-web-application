78a4696bd0a45bb9cd95575061a609d3d930e3c0
Sivagnanalingam Yathavan <yathava-se20015@stu.kln.ac.lk>
2024-07-14 22:46:00 +0530

    template

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var ejs = require('ejs')
 
 var app = express();
 
+app.use(express.static('public'));
+app.set('view engine','ejs'); 
+
 app.listen(8080);
 
 app.get('/',function(req,res){
 
-    res.send("hello");
+    res.render('pages/index');
 
 });
